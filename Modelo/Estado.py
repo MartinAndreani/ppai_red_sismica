99cdf69a8232f80d0bb25cdcf02f0df46396d5ae
Creizz <francocaudanautn@gmail.com>
2025-05-26 02:56:44 -0300

    correccion de metodos creadores y nuevos metodos

--- Modelo/Estado.py
+++ Modelo/Estado.py
@@ -2,3 +2,17 @@ class Estado:
     def __init__(self, ambito: str, nombre: str):
         self.ambito = ambito # str
         self.nombre = nombre # str
+
+    def esAutoDetectado(self):
+        return self.nombre == "AutoDetectado"
+    
+    def esAmbitoDeEventoSismico(self):
+        return self.ambito == "EventoSismico"
+    
+    def esBloqueadoEnRevision(self):
+        return self.nombre == "BloqueadoEnRevision"
+    
+    def esRechazado(self):
+        return self.nombre == "Rechazado"
+    
+    
